47e18ebedabea3e0303dfed5506d637cfb64d07a
elaya38 <elaya.gabay@gmail.com>
2018-05-23 21:32:12 +0300

    clean-up

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express        = require('express'),
       bodyParser     = require('body-parser'),
       deliveryDAO = require('./delivery_server/deliveryDAO'),
       app            = express(),
-      port           = process.env.PORT || 3000;
+      port           = process.env.PORT || 3030;
 
 //POST request parser
 app.use(bodyParser.urlencoded({extended: true}));  
@@ -10,29 +10,28 @@ app.use('/assets', express.static(`${__dirname}/public`));
 app.use(express.static(__dirname + '/public'));
 
 
-app.use(
-    (req,res,next) => {
+//define middleware 
+app.use((req,res,next) => {
         res.set("Content-Type", "application/json");
         next();
     });
 
-
-app.all('/', (req, res) => res.sendFile(`assets/`));
+app.all('/', (req, res) =>
+    res.sendFile(`assets/`));
 
 app.get('/getAllDeliveries', (req, res) => {
     deliveryDAO.getAllDeliveries()
-                  .then(  data => res.json(data))
-
+                  .then(data => res.json(data))
 });
 
 app.post('/getDeliveryByID', (req, res) => {
     deliveryDAO.getDeliveryByID(+req.body.delivery.id)
-                  .then( data => res.json(data));
+                  .then(data => res.json(data));
 });
 
 app.get('/getDeliveriesByRallyPointType', (req, res) => {
     deliveryDAO.getDeliveriesByRallyPointType(req.query.rallypoint)
-                  .then( data => res.json(data));          
+                  .then(data => res.json(data));          
 });
 
 
