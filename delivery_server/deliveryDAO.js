fd205fde4fd899e2f267a871b6aea85cd90e8350
elaya38 <elaya.gabay@gmail.com>
2018-05-24 21:05:14 +0300

    adding comments

--- delivery_server/deliveryDAO.js
+++ delivery_server/deliveryDAO.js
@@ -9,24 +9,25 @@ mongoose.Promise = global.Promise;
 mongoose.connection.on("error", (err) => console.log(`Connection error: ${err}`));
 mongoose.connection.on("open",  ()    => console.log(`Connected to Database `));
 
+//error handling
 function error(description) {
     console.log(`error: ${description}`);
     conf.JSONError['description'] = description;
     return conf.JSONError;
 }
 
-module.exports = class DeliveryDAO {
 
+// methods logics, find for "groups", findOne for unique request (id in that case).
+module.exports = class DeliveryDAO {
     static getAllDeliveries() {
         return Delivery.find()
-                    .catch((err) => err);
+                    .catch(() => error(conf.getAllExeption));
     }
 
     static getDeliveryByID(id) {
         return Delivery.findOne({'delivery.id': id})
                     .catch(() => error(conf.idNotExists));
     }
-
 
     static getDeliveriesByRallyPointType(rally_point) {
       return Delivery.find({'delivery.rally_point.type': rally_point})
--- index.js
+++ index.js
@@ -19,22 +19,26 @@ app.use((req,res,next) => {
 app.all('/', (req, res) =>
     res.sendFile(`assets/`));
 
+//get all deliveries from mlab DB.
 app.get('/getAllDeliveries', (req, res) => {
     deliveryDAO.getAllDeliveries()
                   .then(data => res.json(data))
 });
 
+//get specific delivery by ID.
 app.post('/getDeliveryByID', (req, res) => {
     deliveryDAO.getDeliveryByID(+req.body.delivery.id)
                   .then(data => res.json(data));
 });
 
+//get group of deliveries (or one) specified by their rally point type.
 app.get('/getDeliveriesByRallyPointType', (req, res) => {
     deliveryDAO.getDeliveriesByRallyPointType(req.query.rallypoint)
                   .then(data => res.json(data));          
 });
 
-
+//redirect here in case of an error (for example, when getDeliveryByID request-url is only pressed and not handled right as POST method demands.)
 app.all('*', (req, res) => res.redirect('/assets/error.html'));
 
+//finding avialble port 
 app.listen(port, () => console.log(`Server is listening on ${port}`));
